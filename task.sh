13e87aa467c3718b5e0267f8128c3a9a982a53bf
Maxim Muzafarov <m.muzafarov@gmail.com>
2015-11-17 11:18:00 +0300

    full python path

--- task.sh
+++ task.sh
@@ -4,6 +4,6 @@ cd /usr/local/etc
 rm -rf geo.bak
 cp -r geo geo.bak
 
-/usr/local/bin/ipgeobase_importer
+/usr/local/bin/python /root/ipgeobase_importer/ipgeobase_importer.py
 /usr/local/sbin/nginx -t || (echo "Fail" && cp -r geo.bak/* geo/ && exit 1)
 
