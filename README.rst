6ac74a127b5db9673e6230927ab368cbfe71190c
Maxim Muzafarov <m.muzafarov@gmail.com>
2015-12-02 12:18:04 +0300

    pypi

--- README.rst
+++ README.rst
@@ -1,20 +1,31 @@
-# IpGeoBase importer
+IpGeoBase importer
+==================
 
 Импортер ipgeobase базы русских городов в файлы, понятные для nginx geoip module, с поддержкой кодов регионов РФ.
 
-## Принцип
+Принцип
+-------
 
 1.  Скачивает geo_files.zip с сайта ipgeobase.ru
 2.  Конвертирует базу в два файла:
     +   city.txt, вида: \<start\_ip\>-\<end\_ip\> base64(\<city_name\>);
     +   region.txt, вида: \<start\_ip\>-\<end\_ip\> \<region\_code\>;
+3.  Скачивает списки TOR с torproject и blutmagie.de
+4.  Создает tor.txt, вида: \<start\_ip\>-\<end\_ip\> 1;
 
-## Зависимости
+Установка
+---------
 
-+   Python 2.7
-+   python-requests (`pip install requests`)
+    pip install ipgeobase-importer
+    
+Запуск
+------
 
-## Nginx
+    ipgeobase-importer <output_dir>
+    
+
+Nginx
+-----
 
 ```nginx
 geo $region {
---
+++ setup.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+from distutils.core import setup
+
+from os.path import join, dirname
+
+setup(
+    name='ipgeobase-importer',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/m-messiah/ipgeobase-importer',
+    license='MIT',
+    author='m_messiah',
+    author_email='m.muzafaov@gmail.com',
+    description=u'Импорт ipgeobase и TOR баз '
+                u'в совместимые с nginx geoIP map-файлы',
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    scripts=['ipgeobase-importer'],
+    install_requires=['requests', ],
+    keywords='ipgeobase tor nginx geoip',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: System Administrators',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Utilities',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
