daac36ffc4c2884bd4e616d4f06e67cc26cf76ef
Maxim Muzafarov <m.muzafarov@gmail.com>
2015-12-08 10:51:22 +0300

    v1.1: Обновление названий регионов. Увеличение покрытия адресов в разы

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os.path import join, dirname
 
 setup(
     name='ipgeobase-importer',
-    version='1.0',
+    version='1.1',
     packages=[''],
     url='https://github.com/m-messiah/ipgeobase-importer',
     license='MIT',
