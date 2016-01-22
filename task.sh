e638640311b25b7e13e387c25cee1f25ebdd2b71
Maxim Muzafarov <m.muzafarov@gmail.com>
2016-01-22 12:22:28 +0300

    TZ

--- task.sh
+++ task.sh
@@ -3,6 +3,6 @@
 rm -rf /usr/local/etc/geo.bak
 cp -r /usr/local/etc/geo /usr/local/etc/geo.bak || echo "geo not exists"
 
-/usr/local/bin/ipgeobase-importer /usr/local/etc/geo/
+/usr/local/bin/ipgeobase-importer /usr/local/etc/geo/ 2> /var/log/ipgeobase.log
 /usr/local/sbin/nginx -t || (echo "Fail" >&2 && cp -r /usr/local/etc/geo.bak/* /usr/local/etc/geo/ && exit 1)
 
