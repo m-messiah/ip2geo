e8da8422d8318d593755aa7486e301bbafb8e201
Леутин Александр <a.leutin@infinnity.ru>
2016-06-06 15:35:04 +0500

    GEO_LOCATION now parametrized.

--- task.sh
+++ task.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -e
 
-rm -rf /usr/local/etc/geo.bak
-cp -r /usr/local/etc/geo /usr/local/etc/geo.bak || echo "geo not exists"
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export GEO_LOCATION=/usr/local/etc/geo
 
-/usr/local/bin/ipgeobase-importer /usr/local/etc/geo/ 2> /var/log/ipgeobase.log
-/usr/local/sbin/nginx -t || (echo "Fail" >&2 && cp -r /usr/local/etc/geo.bak/* /usr/local/etc/geo/ && exit 1)
+rm -rf ${GEO_LOCATION}.bak
+cp -r ${GEO_LOCATION} ${GEO_LOCATION}.bak || echo "geo not exists"
+
+/usr/local/bin/ipgeobase-importer ${GEO_LOCATION} 2> /var/log/ipgeobase.log
+nginx -t || (echo "Fail" >&2 && cp -r ${GEO_LOCATION}.bak/* ${GEO_LOCATION} && exit 1)
 
