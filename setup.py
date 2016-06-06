05705793b63bb09df230f2d49d5801071b6a6188
Максим Музафаров <m.muzafarov@gmail.com>
2016-06-06 21:54:52 +0300

    Update version to 1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os.path import join, dirname
 
 setup(
     name='ipgeobase-importer',
-    version='1.4',
+    version='1.5',
     packages=[''],
     url='https://github.com/m-messiah/ipgeobase-importer',
     license='MIT',
@@ -18,27 +18,14 @@ setup(
     install_requires=['requests', ],
     keywords='ipgeobase tor nginx geoip',
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 5 - Production/Stable',
-
-        # Indicate who your project is intended for
         'Intended Audience :: System Administrators',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Utilities',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
