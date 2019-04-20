6ff222b3e752adf4a786eae03fe5b61626114096
m-messiah <m.muzafarov@gmail.com>
2019-04-20 21:30:31 +0300

    Use old strings.Replace(..,-1) for compability

--- maxmind.go
+++ maxmind.go
@@ -162,8 +162,8 @@ func (maxmind *MaxMind) writeMap(locations map[string]geoItem) error {
 		var cityName string
 		var countryName string
 		if maxmind.noBase64 {
-			cityName = "\"" + strings.ReplaceAll(location.City, "\"", "\\\"") + "\""
-			countryName = "\"" + strings.ReplaceAll(location.Country, "\"", "\\\"") + "\""
+			cityName = "\"" + strings.Replace(location.City, "\"", "\\\"", -1) + "\""
+			countryName = "\"" + strings.Replace(location.Country, "\"", "\\\"", -1) + "\""
 		} else {
 			cityName = base64.StdEncoding.EncodeToString([]byte(location.City))
 			countryName = base64.StdEncoding.EncodeToString([]byte(location.Country))
