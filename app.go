9c7c616b05029d0653a20a9df674fee768e2ea57
m-messiah <m.muzafarov@gmail.com>
2019-06-15 15:15:56 +0300

    Refactor similar blocks and bad naming

--- app.go
+++ app.go
@@ -6,23 +6,22 @@ import (
 	"os"
 )
 
+type ip2ProxyConfig struct {
+	Enabled  bool `default:false`
+	Token    string
+	Filename string
+}
+
+// Config - all configuration for tool defined here
 var Config = struct {
 	LogLevel  int    `default:0`
 	OutputDir string `default:"output"`
 	TOR       struct {
 		Enabled bool `default:false`
 	}
-	Ip2Proxy struct {
-		Lite struct {
-			Enabled  bool `default:false`
-			Token    string
-			Filename string
-		}
-		Pro struct {
-			Enabled  bool `default:false`
-			Token    string
-			Filename string
-		}
+	IP2Proxy struct {
+		Lite      ip2ProxyConfig
+		Pro       ip2ProxyConfig
 		PrintType bool `default:false`
 	}
 	MaxMind struct {
@@ -35,7 +34,7 @@ var Config = struct {
 		NoBase64  bool `default:false`
 		NoCountry bool `default:false`
 	}
-	IpGeobase struct {
+	IPGeobase struct {
 		Enabled bool `default:false`
 	}
 }{}
@@ -47,15 +46,15 @@ func main() {
 	version := flag.Bool("version", false, "Print version information and exit")
 
 	flag.StringVar(&Config.OutputDir, "output", "output", "output directory for files")
-	flag.BoolVar(&Config.IpGeobase.Enabled, "ipgeobase", false, "enable ipgeobase generation")
+	flag.BoolVar(&Config.IPGeobase.Enabled, "ipgeobase", false, "enable ipgeobase generation")
 	flag.BoolVar(&Config.TOR.Enabled, "tor", false, "enable tor generation")
-	flag.BoolVar(&Config.Ip2Proxy.Lite.Enabled, "ip2proxy", false, "enable ip2proxy PX4-LITE generation")
-	flag.BoolVar(&Config.Ip2Proxy.Pro.Enabled, "ip2proxy-pro", false, "enable ip2proxy PX4 generation")
-	flag.StringVar(&Config.Ip2Proxy.Lite.Token, "ip2proxy-token", "", "Get token here https://lite.ip2location.com/file-download")
-	flag.StringVar(&Config.Ip2Proxy.Pro.Token, "ip2proxy-pro-token", "", "ip2proxy download token")
-	flag.StringVar(&Config.Ip2Proxy.Lite.Filename, "ip2proxy-lite-filename", "", "Filename of already downloaded ip2proxy-lite db")
-	flag.StringVar(&Config.Ip2Proxy.Pro.Filename, "ip2proxy-pro-filename", "", "Filename of already downloaded ip2proxy db")
-	flag.BoolVar(&Config.Ip2Proxy.PrintType, "ip2proxy-print-type", false, "Print proxy type in map, instead of `1`")
+	flag.BoolVar(&Config.IP2Proxy.Lite.Enabled, "ip2proxy", false, "enable ip2proxy PX4-LITE generation")
+	flag.BoolVar(&Config.IP2Proxy.Pro.Enabled, "ip2proxy-pro", false, "enable ip2proxy PX4 generation")
+	flag.StringVar(&Config.IP2Proxy.Lite.Token, "ip2proxy-token", "", "Get token here https://lite.ip2location.com/file-download")
+	flag.StringVar(&Config.IP2Proxy.Pro.Token, "ip2proxy-pro-token", "", "ip2proxy download token")
+	flag.StringVar(&Config.IP2Proxy.Lite.Filename, "ip2proxy-lite-filename", "", "Filename of already downloaded ip2proxy-lite db")
+	flag.StringVar(&Config.IP2Proxy.Pro.Filename, "ip2proxy-pro-filename", "", "Filename of already downloaded ip2proxy db")
+	flag.BoolVar(&Config.IP2Proxy.PrintType, "ip2proxy-print-type", false, "Print proxy type in map, instead of `1`")
 	flag.BoolVar(&Config.MaxMind.Enabled, "maxmind", false, "enable maxmind generation")
 	flag.IntVar(&Config.MaxMind.IPVer, "ipver", 4, "MaxMind ip version (4 or 6)")
 	flag.StringVar(&Config.MaxMind.Lang, "lang", "ru", "MaxMind city name language")
@@ -76,13 +75,13 @@ func main() {
 	if *veryQuiet {
 		Config.LogLevel = 2
 	}
-	if !(Config.IpGeobase.Enabled || Config.TOR.Enabled || Config.MaxMind.Enabled || Config.Ip2Proxy.Lite.Enabled || Config.Ip2Proxy.Pro.Enabled) {
+	if !(Config.IPGeobase.Enabled || Config.TOR.Enabled || Config.MaxMind.Enabled || Config.IP2Proxy.Lite.Enabled || Config.IP2Proxy.Pro.Enabled) {
 		// By default, generate all maps
-		Config.IpGeobase.Enabled = true
+		Config.IPGeobase.Enabled = true
 		Config.TOR.Enabled = true
 		Config.MaxMind.Enabled = true
-		Config.Ip2Proxy.Lite.Enabled = Config.Ip2Proxy.Lite.Token != "" || Config.Ip2Proxy.Lite.Filename != ""
-		Config.Ip2Proxy.Pro.Enabled = Config.Ip2Proxy.Pro.Token != "" || Config.Ip2Proxy.Pro.Filename != ""
+		Config.IP2Proxy.Lite.Enabled = Config.IP2Proxy.Lite.Token != "" || Config.IP2Proxy.Lite.Filename != ""
+		Config.IP2Proxy.Pro.Enabled = Config.IP2Proxy.Pro.Token != "" || Config.IP2Proxy.Pro.Filename != ""
 	}
 
 	os.MkdirAll(Config.OutputDir, 0755)
@@ -91,7 +90,7 @@ func main() {
 	}
 	goroutinesCount := 0
 	errorChannel := make(chan Error)
-	if Config.IpGeobase.Enabled {
+	if Config.IPGeobase.Enabled {
 		goroutinesCount++
 		i := IPGeobase{
 			OutputDir:  Config.OutputDir,
@@ -125,30 +124,26 @@ func main() {
 		go Generate(&m)
 	}
 
-	if Config.Ip2Proxy.Lite.Enabled {
-		goroutinesCount++
-		o := ip2proxy{
-			Name:       "ip2proxyLite",
-			Token:      Config.Ip2Proxy.Lite.Token,
-			Filename:   Config.Ip2Proxy.Lite.Filename,
-			ErrorsChan: errorChannel,
-			OutputDir:  Config.OutputDir,
-			PrintType:  Config.Ip2Proxy.PrintType,
-		}
-		go o.Get()
-	}
+	for t, ip2ProxyType := range [2]ip2ProxyConfig{Config.IP2Proxy.Lite, Config.IP2Proxy.Pro} {
+		if ip2ProxyType.Enabled {
+			goroutinesCount++
+			var name string
+			if t == 0 {
+				name = "ip2proxyLite"
+			} else {
+				name = "ip2proxyPro"
 
-	if Config.Ip2Proxy.Pro.Enabled {
-		goroutinesCount++
-		o := ip2proxy{
-			Name:       "ip2proxyPro",
-			Token:      Config.Ip2Proxy.Pro.Token,
-			Filename:   Config.Ip2Proxy.Pro.Filename,
-			ErrorsChan: errorChannel,
-			OutputDir:  Config.OutputDir,
-			PrintType:  Config.Ip2Proxy.PrintType,
+			}
+			o := ip2proxy{
+				Name:       name,
+				Token:      ip2ProxyType.Token,
+				Filename:   ip2ProxyType.Filename,
+				ErrorsChan: errorChannel,
+				OutputDir:  Config.OutputDir,
+				PrintType:  Config.IP2Proxy.PrintType,
+			}
+			go o.Get()
 		}
-		go o.Get()
 	}
 
 	for i := 0; i < goroutinesCount; i++ {
